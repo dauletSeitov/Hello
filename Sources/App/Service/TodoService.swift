6897dd56318057cc50cac96069af45609187635a
darwin <seilov.skynet@gmail.com>
2019-04-25 17:10:19 +0600

    init

---
+++ Sources/App/Service/TodoService.swift
@@ -0,0 +1,15 @@
+
+import HTTP
+
+public func routes( ) {
+	
+// // Connect a new client to the supplied hostname.
+// let client = try HTTPClient.connect(hostname: "vapor.codes", on: nil).wait()
+// print(client) // HTTPClient
+// // Create an HTTP request: GET /
+// let httpReq = HTTPRequest(method: .GET, url: "/")
+// // Send the HTTP request, fetching a response
+// let httpRes = try client.send(httpReq).wait()
+// print(httpRes) // HTTPResponse
+
+}
